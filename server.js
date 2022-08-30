2b25743f4de75bea24f4494e693a9a6db2370621
YOSHITAKA Yuichi <yoshitaka@sorich.jp>
2022-08-30 15:31:27 +0900

    port修正

--- server.js
+++ server.js
@@ -2,13 +2,12 @@
 
 const express = require('express');
 
-const PORT = 3000;
-const HOST = '0.0.0.0';
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.get('/', (req, res) => {
   res.send('<h1>Hello, world!</h1>');
 })
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+app.listen(PORT, console.log(`Running at port = ${PORT}`));
+
