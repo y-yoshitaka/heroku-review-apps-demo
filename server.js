ece29729c02873fa5fdaa258543f26d9ea2f2ce4
YOSHITAKA Yuichi <yoshitaka@sorich.jp>
2022-08-30 15:49:10 +0900

    bbbb

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.get('/', (req, res) => {
-  res.send('<h1>Hello, world!</h1>');
+  res.send('<h1>Hello, BBBB!</h1>');
 })
 
 app.listen(PORT, console.log(`Running at port = ${PORT}`));
